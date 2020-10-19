4d1b11f7b480cccd76d2cb132e59257237276516
Sebastian Pino <pino.sebastian16@gmail.com>
2020-10-19 03:58:53 -0500

    Nose

---
+++ Controllers/Controller copy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PracticaCalificada1.Models;
+
+namespace PracticaCalificada1.Controllers
+{
+    public class BonoController : Controller
+    {
+        private readonly ILogger<BonoController> _logger;
+
+        public BonoController(ILogger<BonoController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
